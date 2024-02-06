875ca7393108d13ff08d1b4132df0037cb9bb453
kurbansitterley <kurban.sitterley@nrel.gov>
2024-02-06 16:22:43 -0700

    updated environment

--- watertap3/setup.py
+++ watertap3/setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 setup(
         name='watertap3',
-        version='0.0.1',
-        # packages=['watertap3/build', 'watertap3/wt_units'],
-        packages=find_namespace_packages(),
         url='https://github.com/NREL/WaterTAP3',
-        license='',
-        author='WaterTAP3 Team',
-        author_email='ariel.miara@nrel.gov',
-        description='WaterTAP3 Technoeconomic Tool'
+        description="WaterTAP3 modeling library",
+    keywords="water systems, chemical engineering, process modeling, filtration, desalination, nawi",
+        version='0.0.1.dev0',
+        packages=find_packages(include=("watertap3*")),
+    # install_requires=["pyomo>=6.6.1", "idaes-pse", "numpy", "pandas", "scikit-learn"]
+    install_requires=["watertap==0.11", "ipykernel", "scikit-learn"],
+        author='WaterTAP3 contributors',
+        python_requires=">=3.8",
+        author_email='kurban.sitterley@nrel.gov',
         )
