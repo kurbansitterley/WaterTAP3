df2e5b466c7dcd4114961adb0941e7b86c2e42c1
kurbansitterley <kurban.sitterley@nrel.gov>
2024-02-08 13:22:12 -0700

    add puping energy equation

---
+++ watertap3/watertap3/core/util/pumping_energy.py
@@ -0,0 +1,37 @@
+from pyomo.environ import Param, Var, Expression, Constraint, units as pyunits
+from idaes.core.util.constants import Constants
+
+
+def pumping_energy(blk):
+    unit = blk.unit_model
+    unit_params = unit.unit_params
+    flow = unit.properties_in.flow_vol
+    rho = unit.properties_in.dens_mass
+
+    blk.pump_efficiency = Var(
+        initialize=0.9,
+        bounds=(0, 1),
+        units=pyunits.dimensionless,
+        doc="Pump efficiency",
+    )
+    blk.motor_efficiency = Var(
+        initialize=0.9,
+        bounds=(0, 1),
+        units=pyunits.dimensionless,
+        doc="Motor efficiency",
+    )
+
+    blk.lift_height = Var(
+        initialize=100, bounds=(1, 1e5), units=pyunits.feet, doc="Pump lift height"
+    )
+    blk.power_required = Expression(
+        expr=pyunits.convert(
+            (flow * rho * Constants.acceleration_gravity * blk.lift_height)
+        
+        / (blk.pump_efficiency * blk.motor_efficiency),
+        to_units=pyunits.kW)
+    )
+
+    for k, v in unit_params.items():
+        if hasattr(blk, k):
+            getattr(blk, k).fix(v)
