4dfaad7da3d429f56aa486e2c2c657766ad741b6
kurbansitterley <kurban.sitterley@nrel.gov>
2024-02-13 11:55:44 -0700

    clean up, run black

--- watertap3/watertap3/unit_models/iron_and_manganese_removal.py
+++ watertap3/watertap3/unit_models/iron_and_manganese_removal.py
@@ -170,8 +170,6 @@ class UnitProcessData(WT3UnitProcessSISOData):
             doc="Air-to-water ratio for blower",
         )
 
-        self.handle_unit_params()
-        
         self.air_flow_rate = Var(
             initialize=100,
             bounds=(0, None),
@@ -183,45 +181,8 @@ class UnitProcessData(WT3UnitProcessSISOData):
         def air_flow_rate_constraint(b):
             return b.air_flow_rate == b.air_water_ratio * b.properties_in.flow_vol
 
-    @property 
+        self.handle_unit_params()
+
+    @property
     def default_costing_method(self):
         return cost_iron_and_manganese_removal
-    # def fixed_cap(self):
-    #     # time = self.flowsheet().config.time.first()
-    #     # self.flow_in = pyunits.convert(self.flow_vol_in[time],
-    #     #     to_units=pyunits.m**3/pyunits.hr)
-    #     # self.cap_scaling_exp = 0.7
-    #     # self.cap_scaling_val = 4732 * (pyunits.m**3/pyunits.hour)
-    #     # self.number_of_units = 6
-    #     # self.filter_surf_area = 580 * pyunits.m ** 2
-    #     # self.filter_surf_area = pyunits.convert(self.filter_surf_area,
-    #     #     to_units=pyunits.ft ** 2)
-    #     self.air_water_ratio = 0.001 * pyunits.dimensionless
-    #     self.air_flow_rate = self.air_water_ratio * self.cap_scaling_val
-    #     # self.air_blower_cap =
-
-    #     dual_media_filter_cap =  +  * self.filter_surf_area
-    #     filter_backwash_cap =  +  * self.filter_surf_area
-    #     total_cap_cost = (((self.air_blower_cap + filter_backwash_cap + (dual_media_filter_cap * self.number_of_units))) * self.tpec_tic) * 1E-6
-    #     cap_scaling_factor = self.flow_in / self.cap_scaling_val
-    #     fe_mn_cap = total_cap_cost * cap_scaling_factor ** self.cap_scaling_exp
-    #     return fe_mn_cap
-
-    # def elect(self):
-    #     self.blower_power = 147.8 * (pyunits.hp / (pyunits.m**3/pyunits.hour)) * self.air_flow_rate
-    #     self.blower_power = pyunits.convert(self.blower_power,
-    #         to_units=pyunits.kilowatt)
-    #     electricity = self.blower_power / self.flow_in
-    #     return electricity
-
-    # def get_costing(self):
-    #     '''
-    #     Initialize the unit in WaterTAP3.
-    #     '''
-    #     basis_year = 2014
-    #     tpec_tic = 'TPEC'
-    #     self.costing.fixed_cap_inv_unadjusted = Expression(expr=self.fixed_cap(),
-    #             doc='Unadjusted fixed capital investment')
-    #     self.electricity = Expression(expr=self.elect(),
-    #             doc='Electricity intensity [kWh/m3]')
-    #     financials.get_complete_costing(self.costing, basis_year=basis_year, tpec_tic=tpec_tic)
