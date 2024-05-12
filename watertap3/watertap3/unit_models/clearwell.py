5b6b981a108f45f223407c456d8d503cf75a95dd
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-12 11:34:32 -0600

    update clearwell

--- watertap3/watertap3/unit_models/clearwell.py
+++ watertap3/watertap3/unit_models/clearwell.py
@@ -1,66 +1,98 @@
-from pyomo.environ import Var, Constraint, Expression, units as pyunits
-from watertap3.utils import financials
-from watertap3.core.wt3_unit_pt import WT3UnitProcessPT
+from copy import deepcopy
+
+from pyomo.environ import (
+    Param,
+    Var,
+    check_optimal_termination,
+    value,
+    units as pyunits,
+)
+
+import idaes.logger as idaeslog
+from idaes.core import declare_process_block_class
+from idaes.core.solvers.get_solver import get_solver
+from idaes.core.util.exceptions import InitializationError
+from idaes.core.util.scaling import set_scaling_factor, get_scaling_factor
+
+from watertap.costing.util import make_capital_cost_var
+
+from watertap3.core.wt3_unit_pt import WT3UnitProcessPTData
+
 
 ## REFERENCE
 ## CAPITAL:
-# citation here
+# Kawamura and McGivney
 ## ELECTRICITY:
 # citation here
 
-module_name = 'clearwell'
+__author__ = "Kurban Sitterley"
+module_name = "clearwell"
 
-class UnitProcess(WT3UnitProcessPT):
 
-    def clearwell_setup(self):
-        time = self.flowsheet().config.time.first()
-        
-        self.flow_in = pyunits.convert(self.flow_vol_in[time], 
-            to_units=pyunits.Mgallons/pyunits.hour)
+def cost_clearwell(blk):
 
-        self.storage_time = Var(
+    blk.basis_year = 2007
+    blk.basis_currency = getattr(pyunits, f"USD_{blk.basis_year}")
+
+    blk.capital_cost_base = Var(
+        initialize=1016775,
+        bounds=(0, None),
+        units=blk.basis_currency,
+        doc="Clearwell capital cost base",
+    )
+
+    blk.capital_cost_exp = Var(
+        initialize=0.6836,
+        bounds=(0, None),
+        units=pyunits.dimensionless,
+        doc="Clearwell capital cost exponent",
+    )
+
+    blk.fix_all_vars()
+
+    make_capital_cost_var(blk)
+    blk.costing_package.add_cost_factor(blk, None)
+
+    @blk.Constraint(doc="Capital cost for clearwell")
+    def capital_cost_constraint(b):
+        storage_vol_dim = pyunits.convert(
+            b.unit_model.storage_volume * pyunits.Mgallons**-1,
+            to_units=pyunits.dimensionless,
+        )
+
+        return b.capital_cost == pyunits.convert(
+            b.capital_cost_base * storage_vol_dim**b.capital_cost_exp,
+            to_units=blk.costing_package.basis_currency,
+        )
+
+
+@declare_process_block_class("Clearwell")
+class UnitProcessData(WT3UnitProcessPTData):
+    def build(self):
+        super().build()
+
+        self.storage_time = Param(
             initialize=3,
-            bounds=(0, None),
+            mutable=True,
             units=pyunits.hour,
-            doc='Storage time for clearwell [hr]')
-        self.storage_time.fix(3)
+            doc="Storage time for clearwell",
+        )
 
-        self.storage_vol = Var(
+        self.storage_volume = Var(
             initialize=1,
             bounds=(0, None),
             units=pyunits.Mgallons,
-            doc='Storage volume of clearwell [MG]')
+            doc="Storage volume for clearwell",
+        )
 
-        self.clearwell_capital_A = Var(
-            initialize=1016775,
-            bounds=(0, None),
-            units=pyunits.dimensionless,
-            doc='Clearwell capital A factor')
-        self.clearwell_capital_A.fix(1016775)
+        @self.Constraint(doc="Storage volume calculation")
+        def storage_volume_constraint(b):
+            return b.storage_volume == pyunits.convert(
+                b.properties_in.flow_vol * b.storage_time, to_units=pyunits.Mgallons
+            )
 
-        self.clearwell_capital_B = Var(
-            initialize=0.6836,
-            bounds=(0, None),
-            units=pyunits.dimensionless,
-            doc='Clearwell capital B factor')
-        self.clearwell_capital_B.fix(0.6836)
+        self.handle_unit_params()
 
-        if 'storage_time' in self.unit_params.keys():
-            self.storage_time.fix(self.unit_params['storage_time'])
-
-        self.storage_vol_constr = Constraint(expr=
-            self.storage_vol == self.flow_in * self.storage_time)
-
-
-    def get_costing(self):
-        '''
-        Initialize the unit in WaterTAP3.
-        '''
-        basis_year = 2007
-        self.clearwell_setup()
-        self.costing.fixed_cap_inv_unadjusted = Expression(expr=
-                (self.clearwell_capital_A * self.storage_vol ** self.clearwell_capital_B) * 1E-6,
-                doc='Unadjusted fixed capital investment')
-        self.electricity = Expression(expr=0,
-                doc='Electricity intensity [kWh/m3]')
-        financials.get_complete_costing(self.costing, basis_year=basis_year)
+    @property
+    def default_costing_method(self):
+        return cost_clearwell
