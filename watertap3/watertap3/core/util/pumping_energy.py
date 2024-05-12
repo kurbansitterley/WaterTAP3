db81d0513effee34d5f23e3847660cb031fa4d80
kurbansitterley <kurban.sitterley@nrel.gov>
2024-05-12 11:39:44 -0600

    pumping energy checkpoint

--- watertap3/watertap3/core/util/pumping_energy.py
+++ watertap3/watertap3/core/util/pumping_energy.py
@@ -4,7 +4,7 @@ from idaes.core.util.constants import Constants
 
 def pumping_energy(blk):
     unit = blk.unit_model
-    unit_params = unit.unit_params
+    unit_params = unit.config.unit_params
     flow = unit.properties_in.flow_vol
     rho = unit.properties_in.dens_mass
 
@@ -24,11 +24,14 @@ def pumping_energy(blk):
     blk.lift_height = Var(
         initialize=100, bounds=(1, 1e5), units=pyunits.feet, doc="Pump lift height"
     )
-    blk.power_required = Expression(
+    blk.pumping_power_required = Expression(
         expr=pyunits.convert(
             (flow * rho * Constants.acceleration_gravity * blk.lift_height)
-        
-        / (blk.pump_efficiency * blk.motor_efficiency),
-        to_units=pyunits.kW)
+            / (blk.pump_efficiency * blk.motor_efficiency),
+            to_units=pyunits.kW,
+        )
     )
 
+    blk.fix_all_vars()
+
+    blk.costing_package.cost_flow(blk.pumping_power_required, "electricity")
